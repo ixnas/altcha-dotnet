c7bd6447a8c4396afb7ebfa7bbe479b280b181d0
Sjoerd Scheffer <sjoerd@b00n.org>
2025-03-21 12:51:43 +0100

    Cryptographically secure RNG

--- Ixnas.AltchaNet/Internal/ProofOfWork/Generation/RandomNumberGenerator.cs
+++ Ixnas.AltchaNet/Internal/ProofOfWork/Generation/RandomNumberGenerator.cs
@@ -5,6 +5,10 @@ namespace Ixnas.AltchaNet.Internal.ProofOfWork.Generation
     internal class RandomNumberGenerator
     {
         public int Max { get; }
+#if !NET8_0_OR_GREATER
+        private readonly System.Security.Cryptography.RandomNumberGenerator _generator =
+            System.Security.Cryptography.RandomNumberGenerator.Create();
+#endif
         private readonly int _min;
 
         public RandomNumberGenerator(int min, int max)
@@ -15,7 +19,24 @@ namespace Ixnas.AltchaNet.Internal.ProofOfWork.Generation
 
         public int Generate()
         {
-            return new Random().Next(_min, Max + 1);
+#if NET8_0_OR_GREATER
+            return System.Security.Cryptography.RandomNumberGenerator.GetInt32(_min, Max + 1);
+#else
+            // Algorithm based on https://gist.github.com/niik/1017834
+            var diff = (long)Max + 1 - _min;
+            var upperBound = uint.MaxValue / diff * diff;
+
+            var buffer = new byte[sizeof(uint)];
+            uint number;
+            do
+            {
+                _generator.GetBytes(buffer);
+                number = BitConverter.ToUInt32(buffer, 0);
+            }
+            while (number >= upperBound);
+
+            return (int)(_min + number % diff);
+#endif
         }
     }
 }
